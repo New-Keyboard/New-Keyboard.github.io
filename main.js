ba0dca35c89cec5e14837d214e474d3282b3199f
New-Keyboard <69916963+New-Keyboard@users.noreply.github.com>
2020-08-19 21:11:14 +0300

    main

---
+++ main.js
@@ -0,0 +1,29 @@
+//JavaScript
+
+const selectElement = (element) => document.querySelector(element);
+
+selectElement('.mobile-menu').addEventListener('click', ()=>{
+    selectElement('header').classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
